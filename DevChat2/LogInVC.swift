d589c602a971d54a1868fbb3cb921986f7a3084b
tintedsky <hongbonew@gmail.com>
2017-09-30 22:43:22 -0400

    tiime_5:57_Sep_30_before_bed

--- DevChat2/LogInVC.swift
+++ DevChat2/LogInVC.swift
@@ -9,27 +9,24 @@
 import UIKit
 
 class LogInVC: UIViewController {
-
+    
+    @IBOutlet weak var emailField: RoundTextField!
+    @IBOutlet weak var passwordField: RoundTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func loginPressed(_ sender: Any) {
+        if let email = emailField.text, let pass = passwordField.text, email.characters.count > 0 && pass.characters.count > 0 {
+            // Call the login service
+            
+        }else{
+            let alert = UIAlertController(title: "Username and Password Required", message: "You must enter both an username and password", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            present(alert, animated: true, completion: nil)
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
